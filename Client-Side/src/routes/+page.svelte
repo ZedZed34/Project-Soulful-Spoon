5a7994a6d14bc8d8f6399d824c3d236f488caea8
ZedZed34 <haun070@aucklanduni.ac.nz>
2025-02-08 14:40:16 +0800

    v1.32

--- Client-Side/src/routes/+page.svelte
+++ Client-Side/src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to Harshikaa's World</h1>
 <h2>Life is good with Svelte</h2>
-<h2>Harshikaa is an expert in CSS</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
