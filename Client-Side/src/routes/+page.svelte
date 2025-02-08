8f326dac581d9134038434070ebed2613e708d81
strawberryscow <ayubalisha03@gmail.com>
2025-02-08 15:15:13 +0800

    v1.35

--- Client-Side/src/routes/+page.svelte
+++ Client-Side/src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to Billy's World</h1>
+<h1>Welcome to Alisha's World</h1>
 <h2>Life is good with Svelte</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
