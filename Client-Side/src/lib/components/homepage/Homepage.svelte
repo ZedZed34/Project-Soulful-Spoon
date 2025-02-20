f4c6ce98ad61305852a5fadcf02a3b1cddcfd070
strawberryscow <ayubalisha03@gmail.com>
2025-02-20 19:00:09 +0800

    homepage header

---
+++ Client-Side/src/lib/components/homepage/Homepage.svelte
@@ -0,0 +1,12 @@
+<scipt>
+    import "$lib/css/homepage.css"; //import homepage styles
+</scipt>
+
+<section class="header">
+    <h1>Welcome to Soulful Spoon</h1>
+    <h2>Wholesome, Easy, Delicious for Everyone!</h2>
+</section>
+
+<section class="content">
+    <p>Discover trending recipes, tips, and more.</p>
+</section>
