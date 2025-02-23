a9d61bcc18b4b1d5e57c46f84dbbcfb383cd5b6c
strawberryscow <ayubalisha03@gmail.com>
2025-02-23 13:34:55 +0800

    homepage html nav bar

---
+++ Client-Side/src/lib/components/homepage/Homepage.svelte
@@ -0,0 +1,22 @@
+</script>
+
+<!-- Navbar -->
+<nav class="navbar">
+   <div class="logo-container">
+       <img src={logo} alt="Soulful Spoon Logo" class="logo" />
+       <span class="site-name">Soulful Spoon</span>
+   </div>
+   <ul class="nav-buttons">
+       <li><a href="/">Home</a></li>
+       <li><a href="/recipes">Recipes</a></li>
+       <li><a href="/About us">About us</a></li>
+   </ul>
+   <div class="add-button">
+       <button title="Add New">+</button>
+   </div>
+</nav>
+
+<main>
+   <h1>Welcome to Soulful Spoon</h1>
+   <!-- <p>Discover trending recipes, tips, and more.</p> -->
+</main>
