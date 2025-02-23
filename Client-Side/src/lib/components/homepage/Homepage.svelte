0a041bc9f9733cd04f6e1a94cb2a9df473082ba2
strawberryscow <ayubalisha03@gmail.com>
2025-02-23 13:30:08 +0800

    changed logo image

--- Client-Side/src/lib/components/homepage/Homepage.svelte
+++ Client-Side/src/lib/components/homepage/Homepage.svelte
@@ -1,12 +1,25 @@
-<scipt>
-    import "$lib/css/homepage.css"; //import homepage styles
-</scipt>
+<script>
+    import '$lib/css/homepage.css'; //import homepage styles
+    import logo from '$lib/components/images/Logo.png'; 
+</script>
 
-<section class="header">
+ <!-- Navbar -->
+<nav class="navbar">
+    <div class="logo-container">
+        <img src={logo} alt="Soulful Spoon Logo" class="logo" />
+        <span class="site-name">Soulful Spoon</span>
+    </div>
+    <ul class="nav-buttons">
+        <li><a href="/">Home</a></li>
+        <li><a href="/recipes">Recipes</a></li>
+        <li><a href="/About us">About us</a></li>
+    </ul>
+    <div class="add-button">
+        <button title="Add New">+</button>
+    </div>
+</nav>
+
+<main>
     <h1>Welcome to Soulful Spoon</h1>
-    <h2>Wholesome, Easy, Delicious for Everyone!</h2>
-</section>
-
-<section class="content">
-    <p>Discover trending recipes, tips, and more.</p>
-</section>
+    <!-- <p>Discover trending recipes, tips, and more.</p> -->
+</main>
---
+++ Client-Side/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Homepage from "$lib/components/Homepage/Homepage.svelte";
+  </script>
+
+  <Homepage />
+
+<svelte:head>
+    <title>Home Page</title>
+  </svelte:head>
