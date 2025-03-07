cf0f1eb04a054ca3c97f9af70e724834bee35357
ambermayishere <xxishitabxx@gmail.com>
2025-03-07 19:50:17 +0800

    corrected the +page.svelte for Recipepage Routes

--- Client-Side/src/routes/RecipePage/+page.svelte
+++ Client-Side/src/routes/RecipePage/+page.svelte
@@ -1,17 +1,9 @@
 <script>
-<<<<<<<< HEAD:Client-Side/src/routes/recipes/+page.svelte
-	import Recipes from "$lib/components/Recipes/Recipes.svelte";
-========
-	import Recipepage from "$lib/components/RecipePage/Recipepage.svelte";
->>>>>>>> alisha:Client-Side/src/routes/RecipePage/+page.svelte
-  </script>
-  
-  <svelte:head>
-    <title>Recipe Page</title>
-  </svelte:head>
-  
-<<<<<<<< HEAD:Client-Side/src/routes/recipes/+page.svelte
-  <Recipes/>
-========
-  <Recipepage/>
->>>>>>>> alisha:Client-Side/src/routes/RecipePage/+page.svelte
+  import Recipes from "$lib/components/Recipes/Recipes.svelte";
+</script>
+
+<svelte:head>
+  <title>Recipes</title>
+</svelte:head>
+
+<Recipes/>
